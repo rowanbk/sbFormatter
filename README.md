77d8ebc6e5cce71088c139e80bba484f6b0628c5
BURNS-KIRKNESS Rowan <bk@rowan.engineer>
2019-01-07 10:18:18 +0900

    Added Caleb style support amoung other upgrades

--- README.md
+++ README.md
@@ -1,5 +1,85 @@
 # sbFormatter
 Turn sideboard documents in printable SB maps
 
+# Requirements:
+install the python library "argparser" with "pip install argparse" or a package manager of your choice
+
+
+# CyrusCG
 Using CyrusCG's sideboard plan formatting from google docs this will create a more readable printer friendly version. Make sure to always use full card names (eg. Empty the Warrens not ETW). If you do want to abbreviate a card name add it to the abbrs.txt file in the format: Proper Name/Short. Eg:
 Empty the Warrens/ETW
+
+Input:
+
+~ Deck Name
++1 Card1 +2 Card2
+-3 Card3
+
+Output:
+
+|--------Deck Name--------|
+| -3 Card3       +2 Card2 |
+|	 	 +1 Card1 |
+
+# Caleb
+To use Caleb's formatting from his website "adventuringgear.wordpress.com" the following steps are needed:
+Input:
+
+~ Deck Name
+In: 1 Card1 2 Card2
+Out: 3 Card3
+
+Output:
+
+|--------Deck Name--------|
+| -3 Card3       +2 Card2 |
+|	 	 +1 Card1 |
+
+1. copy his sideboard plan to a .txt document
+2. copy your decklist to the top of that document eg.
+	4 Spirebluff Canal
+	4 ...
+	SIDEBOARD
+	2 Empty the Warrens
+	1 ...
+3. call the program with the -c argument "python sbFormatter.py -c CalebsGuide.txt"
+
+#Options
+-c : use Caleb formatting instead of Cyrus formatting
+-f N : alternate format where the guide shows the 15 cards that should be removed instead of ins and outs. N is the number of columns to print with
+	eg. python sbFormatter.py -f 3 CyrusGuide.txt
+	output:
+	...
+	|~Elves~            |~RB Reanimator~    |~Grixis Control~   |
+	|Decay              |Decay              |Decay              |
+	|Decay              |Decay              |Decay              |
+	|Xantid             |Xantid             |Xantid             |
+	|Xantid             |Xantid             |Xantid             |
+	|Hurkyl's           |Hurkyl's           |Hurkyl's           |
+	|Hurkyl's           |Hurkyl's           |Hurkyl's           |
+	|Truth              |Truth              |Truth              |
+	|Truth              |Truth              |Truth              |
+	|Truth              |Truth              |Truth              |
+	|Massacre           |Massacre           |CoV                |
+	|Empty              |Empty              |CoV                |
+	|Duress             |Seize              |Massacre           |
+	|Duress             |Seize              |Empty              |
+	|Swamp              |Swamp              |Petal              |
+	|PiF                |Preordain          |Cabal              |
+	etc...
+
+-i : do not abbreviate card names
+	eg. python sbFormatter.py -i CyrusGuide.txt
+	output:
+	...
+	|-----------------Stoneblade-----------------|
+	| -1 Rain of Filth      +2 Xantid Swarm      |
+	| -1 Past in Flames     +2 Abrupt Decay      |
+	| -1 Lotus Petal        +1 Massacre          |
+	| -1 Dark Petition                           |
+	| -1 Cabal Ritual                            |
+	|                                            |
+	etc...
+
+-o : save to a file other than "printout.txt"
+	eg. python sbFormatter.py -o saveme.txt
--- sbFormatter.py
+++ sbFormatter.py
@@ -1,5 +1,6 @@
 import re
 import argparse
+from math import *
 
 class Deck(object):
 
@@ -7,12 +8,14 @@ class Deck(object):
         self.name = name
         self.cardsIn = []
         self.cardsOut = []
+        self.maybeIn = []
+        self.maybeOut = []
 
     def __lt__(self,other):
         return self.rows() > other.rows()
 
     def rows(self):
-       return max(len(self.cardsIn),len(self.cardsOut))
+       return max(len(self.cardsIn)+len(self.maybeIn),len(self.cardsOut)+len(self.maybeOut))
 
     def sortCards(self):
        self.cardsIn.sort(reverse=True)
@@ -24,24 +27,55 @@ class Deck(object):
             count += int(card[1])
         for card in self.cardsOut:
             count -= int(card[1])
-        if count > 0:
+        if len(self.maybeOut) == 0 and count > 0:
             print(self.name,count,"more in than out")
-        if count < 0:
+        if len(self.maybeIn) == 0 and count < 0:
             print(self.name,-1*count,"more out than in")
 
+    def shortenNames(self,noshorten):
+        for s in [self.cardsIn,self.cardsOut,self.maybeIn,self.maybeOut]:
+            for i in range(len(s)):
+                name = s[i][3:].lower()
+                if name in shortNames and not noshorten:
+                    s[i] = s[i][:3]+shortNames[name]
+                elif not name in listed and not noshorten:
+                    print("No abbr for:",name,self.name)
+                    listed.append(name)
 
-    def shortenNames(self):
-        nms = open("abbrs.txt","r")
-        shortNames = {}
-        for line in nms:
-            splitLine = line.split('/')
-            shortNames[splitLine[0]] = splitLine[1].rstrip()
-        for i in range(len(self.cardsIn)):
-            if self.cardsIn[i][3:] in shortNames:
-                self.cardsIn[i] = self.cardsIn[i][:3]+shortNames[self.cardsIn[i][3:]]
-        for i in range(len(self.cardsOut)):
-            if self.cardsOut[i][3:] in shortNames:
-                self.cardsOut[i] = self.cardsOut[i][:3]+shortNames[self.cardsOut[i][3:]]
+def CWStoCyrus(f):
+    lines = [""]
+    for line in f:
+        curr = ""
+        line = line.rstrip('\n')
+        beginning = line.split(' ',1)[0].lower()
+        if "in:" in beginning or "out:" in beginning:
+            if "in" in beginning:
+                add_char = "+"
+            else:
+                add_char = "-"
+            first = True
+            for sec in line.split()[1:]:
+                if len(curr) > 0 and sec.isdigit():
+                    if not first:
+                        lines[-1] += ", "+curr
+                    else:
+                        first = False
+                        lines[-1] += curr
+                    curr = add_char+sec
+                elif sec.isdigit():
+                    curr += add_char+sec.rstrip(',')
+                else:
+                    curr += " "+sec.rstrip(',')
+            lines[-1] += ", "+curr
+            lines.append("")
+        elif beginning.isdigit() or beginning == "sideboard":
+            lines.append(line)
+        elif len(beginning) > 0:
+            lines.append("~"+line+"\n")
+            lines.append("")
+
+    return lines
+
 
 def groupByPlan(decks):
     groupedDecks = []
@@ -58,7 +92,7 @@ def groupByPlan(decks):
                     maxSim = overlap
                     maxInd = i
         groupedDecks.append(decks[maxInd])
-        del decks[maxInd] 
+        del decks[maxInd]
 
     return groupedDecks
 
@@ -67,21 +101,42 @@ def groupByPlan(decks):
 parser = argparse.ArgumentParser()
 parser.add_argument('path',nargs='?')
 parser.add_argument('-o',nargs='?', default="printout.txt")
+parser.add_argument('-i',action='store_true')
+parser.add_argument('-f',nargs='?', default=False)
+parser.add_argument('-c',action='store_true')
 args = parser.parse_args()
 
-f = open(vars(args)['path'],"r")
 out = open(vars(args)['o'],"w")
+noshorten = vars(args)['i']
+cws = vars(args)['c']
+allout = vars(args)['f']
+nms = open("abbrs.txt","r")
+shortNames = {}
+with open(vars(args)['path'],"r") as f:
+    if cws:
+        file_lines = CWStoCyrus(f)
+    else:
+        file_lines = [line for line in f]
+for line in nms:
+    splitLine = line.split('/')
+    shortNames[splitLine[0].lower()] = splitLine[1].rstrip()
 pline = ""
 decks = []
 cards = []
 duplicate = False
-for line in f:
+maindeck = 1
+sidestart = 0
+for line in file_lines:
     if re.search(r'[-+] [1-4]',line):
         line = re.sub(r'(?<=[-+]) (?=[1-4])','', line)
-    if re.search(r'^[1-4] [A-Z][A-Za-z ]',line):
-        line = line[2:]
-        line = line.rstrip('\n')
-        cards.append(line)
+    if "sideboard" in line.lower():
+        maindeck = 0
+    elif re.search(r'^[1-4] [A-Z][A-Za-z ]',line):
+        line = line.lower().rstrip('\n')
+        if not "sideboard" in line:
+            num,name = line.split(" ",1)
+            cards += int(num)*[name]
+        sidestart += int(num)*maindeck
     elif '~' in line:
         line = line[:line.find(':')]
         line = line.lstrip("~ ")
@@ -95,13 +150,27 @@ for line in f:
         words = re.split('[, ]',line)
         change = ""
         name = ""
+        optional = False
+        option = False
         for word in words:
-            word = word.rstrip(',./\n ')
-            if re.search(r'^[-+][1-4]$',word):
+            word = word.rstrip(',./:\n ')
+            if word == "OPTION":
+                optional = True
+            elif re.search(r'^[-+][1-4]$',word):
                 if re.search(r'^[-][1-4]',change):
-                    decks[-1].cardsOut.append(change+name)
+                    if option:
+                        decks[-1].maybeOut.append(change+name)
+                    else:
+                        decks[-1].cardsOut.append(change+name)
+                        if optional:
+                            option = True
                 elif re.search(r'^[+][1-4]',change):
-                    decks[-1].cardsIn.append(change+name)
+                    if option:
+                        decks[-1].maybeIn.append(change+name)
+                    else:
+                        decks[-1].cardsIn.append(change+name)
+                        if optional:
+                            option = True
                 name = ""
                 change = word
             elif name.lstrip() in cards:
@@ -109,23 +178,30 @@ for line in f:
             elif len(word)> 1:
                 name+=' '+word
 
-
         if re.search(r'^[-][1-4]',change):
-            decks[-1].cardsOut.append(change+name)
+            if optional:
+                decks[-1].maybeOut.append(change+name)
+            else:
+                decks[-1].cardsOut.append(change+name)
         elif re.search(r'^[+][1-4]',change):
-            decks[-1].cardsIn.append(change+name)
+            if optional:
+                decks[-1].maybeIn.append(change+name)
+            else:
+                decks[-1].cardsIn.append(change+name)
 
     if duplicate:
         break
 
-f.close()
-
 decks.sort()
 #decks = groupByPlan(decks.copy())
+if len(decks) == 0 or len(cards) == 0:
+    print("Decklist or argument error")
+    exit(1)
 
 max_name_width = 0
+listed = []
 for deck in decks:
-    deck.shortenNames()
+    deck.shortenNames(noshorten != False)
     if (len(deck.name)//2)+1 > max_name_width:
         max_name_width = (len(deck.name)//2)+1
     for card in deck.cardsIn + deck.cardsOut:
@@ -139,117 +215,138 @@ max_name_width += 2
 card_rows = []
 max_block = 0
 decklimiter = '|'
-boarder = '.'
+foldSpace = ' '
 name_acc = '-'
-row_dev = " "
+bolden = '~'
+optional = '*'
+rowCount = 6
+maxRowCount = 20
+colwidth = max([len(shortNames[card]) for card in shortNames]+[len(deck.name)+3 for deck in decks])
+cols = int(allout)
 
+if allout != False:
+    for r in range(ceil(len(decks)/cols)):
+        decks = groupByPlan(decks)
+        totalout = []
+        for c in range(cols):
+            i = (cols*r)+c
+            if i < len(decks):
+                out.write((decklimiter+bolden+decks[i].name+bolden).ljust(colwidth))
+                totalin = []
+                totalout.append([])
+                for line in decks[i].cardsIn:
+                    totalin += int(line[1])*[line[3:].lower()]
+                for card in cards[sidestart:]:
+                    if not noshorten:
+                        card = shortNames[card]
+                    if card.lower() in totalin:
+                        totalin.remove(card.lower())
+                    else:
+                        totalout[c].append(card)
+                for line in decks[i].cardsOut:
+                    totalout[c] += int(line[1])*[line[3:]]
+            else:
+                out.write((decklimiter).ljust(colwidth))
+        out.write(decklimiter+'\n')
+        for num in range(15):
+            for c in range(cols):
+                if c < len(totalout):
+                    out.write((decklimiter+totalout[c][num]).ljust(colwidth))
+                else:
+                    out.write(decklimiter.ljust(colwidth))
+            out.write(decklimiter+'\n')
+        out.write(decklimiter+(((cols*colwidth)-1)*name_acc)+decklimiter+'\n')
 
-for i in range(0,len(decks),3):
-    j = i+1
-    k = j+1
-    maxRows = decks[i].rows()
-    printString = ""
-    if(j<len(decks)):
-        maxRows = max(maxRows,decks[j].rows())
-    if(k<len(decks)):
-        maxRows = max(maxRows,decks[k].rows())
-    pad_len = max_name_width - (len(decks[i].name) // 2)
-    printString += (decklimiter
-                    + name_acc*(pad_len-(len(decks[i].name)%2))
-                    + decks[i].name
-                    + name_acc*(pad_len))
-    if(j<len(decks)):
-        pad_len = max_name_width - (len(decks[j].name) // 2)
+else:
+    for i in range(0,len(decks),3):
+        j = i+1
+        k = j+1
+        maxRows = decks[i].rows()
+        printString = ""
+
+        if rowCount >= maxRowCount:
+            for r in range(max(1,(25 - rowCount))):
+                out.write((foldSpace*((6*max_name_width)+5))+'\n')
+            rowCount = 0
+
+        if(j<len(decks)):
+            maxRows = max(maxRows,decks[j].rows())
+        if(k<len(decks)):
+            maxRows = max(maxRows,decks[k].rows())
+        pad_len = max_name_width - (len(decks[i].name) // 2)
         printString += (decklimiter
-                        + name_acc*(pad_len)
-                        + decks[j].name
-                        + name_acc*(pad_len+(1+len(decks[j].name))%2))
-    else:
-        printString+=decklimiter+(1+2*max_name_width)*' '
-    if(k<len(decks)):
-        pad_len = max_name_width - (len(decks[k].name) // 2)
-        printString += (decklimiter
-                        + name_acc*(pad_len-(len(decks[k].name)%2))
-                        + decks[k].name
-                        + name_acc*(pad_len)
-                        +decklimiter)
-    else:
-        printString+=decklimiter+2*max_name_width*' '+decklimiter
+                        + name_acc*(pad_len-(len(decks[i].name)%2))
+                        + decks[i].name
+                        + name_acc*(pad_len))
+        if(j<len(decks)):
+            pad_len = max_name_width - (len(decks[j].name) // 2)
+            printString += (decklimiter
+                            + name_acc*(pad_len)
+                            + decks[j].name
+                            + name_acc*(pad_len+(1+len(decks[j].name))%2))
+        else:
+            printString+=decklimiter+(1+2*max_name_width)*' '
+        if(k<len(decks)):
+            pad_len = max_name_width - (len(decks[k].name) // 2)
+            printString += (decklimiter
+                            + name_acc*(pad_len-(len(decks[k].name)%2))
+                            + decks[k].name
+                            + name_acc*(pad_len)
+                            +decklimiter)
+        else:
+            printString+=decklimiter+2*max_name_width*' '+decklimiter
 
-    out.write(printString+'\n')
-    for r in range(maxRows+1):
-        printString = decklimiter+' '
-        if len(decks[i].cardsOut) > r:
-            printString += decks[i].cardsOut[r]+ ' '*(max_name_width-len(decks[i].cardsOut[r]))
-        else:
-            printString+=max_name_width*' '
-        if len(decks[i].cardsIn) > r:
-            printString += decks[i].cardsIn[r]+ ' '*(max_name_width-1-len(decks[i].cardsIn[r]))
-        else:
-            printString+= (max_name_width-1)*' '
-
-        printString+=decklimiter+' '
-        if j<len(decks) and len(decks[j].cardsOut) > r:
-            printString += decks[j].cardsOut[r]+ ' '*(max_name_width-len(decks[j].cardsOut[r]))
-        else:
-            printString+=max_name_width*' '
-        if j<len(decks) and len(decks[j].cardsIn) > r:
-            printString += decks[j].cardsIn[r]+ ' '*(max_name_width-len(decks[j].cardsIn[r]))
-        else:
-            printString+= (max_name_width)*' '
-
-        printString+=decklimiter+' '
-        if k<len(decks) and len(decks[k].cardsOut) > r:
-            printString += decks[k].cardsOut[r]+ ' '*(max_name_width-len(decks[k].cardsOut[r]))
-        else:
-            printString+=max_name_width*' '
-        if k<len(decks) and len(decks[k].cardsIn) > r:
-            printString += decks[k].cardsIn[r]+ ' '*(max_name_width-1-len(decks[k].cardsIn[r]))
-        else:
-            printString+= (max_name_width-1)*' '
-        printString+=decklimiter
         out.write(printString+'\n')
+        rowCount+=1
+        for r in range(maxRows+1):
+            printString = decklimiter+" "
+            if len(decks[i].cardsOut) > r:
+                printString += decks[i].cardsOut[r]+ ' '*(max_name_width-len(decks[i].cardsOut[r]))
+            elif len(decks[i].cardsOut)+len(decks[i].maybeOut) > r:
+                maybeR = r-len(decks[i].cardsOut)
+                printString += decks[i].maybeOut[maybeR]+optional+' '*(max_name_width-1-len(decks[i].maybeOut[maybeR]))
+            else:
+                printString+=(max_name_width)*' '
+            if len(decks[i].cardsIn) > r:
+                printString += decks[i].cardsIn[r]+ ' '*(max_name_width-1-len(decks[i].cardsIn[r]))
+            elif len(decks[i].cardsIn)+len(decks[i].maybeIn) > r:
+                maybeR = r-len(decks[i].cardsIn)
+                printString += decks[i].maybeIn[maybeR]+optional+' '*(max_name_width-2-len(decks[i].maybeIn[maybeR]))
+            else:
+                printString+= (max_name_width-1)*' '
 
+            printString+=decklimiter+' '
+            if j<len(decks) and len(decks[j].cardsOut) > r:
+                printString += decks[j].cardsOut[r]+ ' '*(max_name_width-len(decks[j].cardsOut[r]))
+            elif j<len(decks) and len(decks[j].cardsOut)+len(decks[j].maybeOut) > r:
+                maybeR = r-len(decks[j].cardsOut)
+                printString += decks[j].maybeOut[maybeR]+optional+' '*(max_name_width-1-len(decks[j].maybeOut[maybeR]))
+            else:
+                printString+=(max_name_width)*' '
+            if j<len(decks) and len(decks[j].cardsIn) > r:
+                printString += decks[j].cardsIn[r]+ ' '*(max_name_width-len(decks[j].cardsIn[r]))
+            elif j<len(decks) and len(decks[j].cardsIn)+len(decks[j].maybeIn) > r:
+                maybeR = r-len(decks[j].cardsIn)
+                printString += decks[j].maybeIn[maybeR]+optional+' '*(max_name_width-1-len(decks[j].maybeIn[maybeR]))
+            else:
+                printString+= (max_name_width)*' '
 
-
-
-
-
-
-exit(0)
-for i in range(len(decks)):
-    curr_block = 0
-    deck = decks[i]
-    if i%3 == 0 and i>0:
-        out.write(boarder+name_row[:-1]+boarder+'\n')
-        for row in card_rows:
-            out.write(boarder+row[:-1]+boarder+'\n')
-        out.write(boarder+row_dev*max_name_width*2+decklimiter)
-        out.write(row_dev*max_name_width*2+decklimiter)
-        out.write(row_dev*max_name_width*2+boarder+'\n')
-        name_row = ""
-        card_rows = []
-        max_block = 0
-    pad_len = max_name_width - (len(deck.name) // 2)
-    name_row += name_acc*(pad_len-(len(deck.name)%2))+deck.name+ name_acc*pad_len +decklimiter
-    for crn in range(deck.rows()):
-        if crn >= len(card_rows):
-            card_rows.append("") 
-            for j in range(i%3):
-                card_rows[crn] += ' '*(max_name_width*2) + decklimiter
-        if crn < len(deck.cardsOut):
-            card_rows[crn] += ' '+deck.cardsOut[crn]
-            card_rows[crn] += ' '*(max_name_width-1-len(deck.cardsOut[crn]))
-        else:
-            card_rows[crn] += ' '*(max_name_width)
-        if crn < len(deck.cardsIn):
-            card_rows[crn] += deck.cardsIn[crn]
-            card_rows[crn] += ' '*(max_name_width-len(deck.cardsIn[crn]))+decklimiter
-        else:
-            card_rows[crn] += ' '*(max_name_width)+decklimiter
-        curr_block = crn
-    if curr_block >= max_block:
-        max_block = curr_block
-    else:
-        card_rows[max_block] += ' '*(max_name_width*2)+decklimiter
-out.close()
+            printString+=decklimiter+' '
+            if k<len(decks) and len(decks[k].cardsOut) > r:
+                printString += decks[k].cardsOut[r]+ ' '*(max_name_width-len(decks[k].cardsOut[r]))
+            elif k<len(decks) and len(decks[k].cardsOut)+len(decks[k].maybeOut) > r:
+                maybeR = r-len(decks[k].cardsOut)
+                printString += decks[k].maybeOut[maybeR]+optional+' '*(max_name_width-1-len(decks[k].maybeOut[maybeR]))
+            else:
+                printString+=(max_name_width)*' '
+            if k<len(decks) and len(decks[k].cardsIn) > r:
+                printString += decks[k].cardsIn[r]+ ' '*(max_name_width-1-len(decks[k].cardsIn[r]))
+            elif k<len(decks) and len(decks[k].cardsIn)+len(decks[k].maybeIn) > r:
+                maybeR = r-len(decks[k].cardsIn)
+                printString += decks[k].maybeIn[maybeR]+optional+' '*(max_name_width-2-len(decks[k].maybeIn[maybeR]))
+            else:
+                printString+= (max_name_width-1)*' '
+            printString+=decklimiter
+            out.write(printString+'\n')
+            rowCount+=1
+    out.write((name_acc*((6*max_name_width)+5))+'\n')
